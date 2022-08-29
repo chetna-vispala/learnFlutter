f014053dfc29fe55af3469154f1b4921aa42dfaa
chetna-vispala <chetna@vispala.com>
2022-08-29 15:33:47 +0530

    catalog for product

---
+++ lib/models/catalog.dart
@@ -0,0 +1,22 @@
+
+class Item{
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+}
+final product = [
+  Item(
+    id: "ch033",
+    name: "iPhone 12 pro",
+    desc: "",
+    price: 109900,
+    color: "Pacific Blue",
+    image: "https://rukminim1.flixcart.com/image/416/416/kg8avm80/mobile/u/c/d/apple-iphone-12-pro-dummyapplefsn-original-imafwgbrzxg3nggd.jpeg?q=70",
+
+  ),
+];
